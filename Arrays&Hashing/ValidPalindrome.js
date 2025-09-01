23fc77d3ddb21aef2560b65cd00643c838a6159b
krissaint <mynamecamranh@gmail.com>
2025-09-01 16:07:52 -0700

    ‘Message’

---
+++ Arrays&Hashing/ValidPalindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let left = 0;
+    let right = s.length -1;
+
+    while (left < right) {
+        if (s[left] !== s[right]){
+            return false
+        } 
+        left++;
+        right--;
+    }
+    return true;
+
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+console.log(isPalindrome("race a car")); // false
